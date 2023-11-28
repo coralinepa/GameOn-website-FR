47ada5091837c15515d3ea6ee937c2a25ecbe91d
Coraline <122123965+coralinepa@users.noreply.github.com>
2023-11-28 09:55:11 +0100

    fix: remove typo

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -167,7 +167,6 @@ function validateForm(values) {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
- fix/correct-file-error
   // Création d'une constante formData pour créer une instance formData qui contient les valeurs du formulaire.
   const formData = new FormData(form);
 
